2539302bd21dc737447a7dc112ccd26b888f53d0
Vincent Toups <Vincent Toups>
2021-07-15 11:48:10 -0400

    Improved start script.

--- start-env.sh
+++ start-env.sh
@@ -18,9 +18,15 @@ function box_out()
 }
 
 docker build . -t 611
+
+MODE=$1
+
+if [ -z "$MODE" ] || [ "$MODE" == "rstudio" ];
+then
 export R_PASS=$(strings /dev/urandom | grep -o '[[:alnum:]]' | head -n 30 | tr -d '\n'; echo)
 docker run --rm \
  -p 8787:8787 \
+ -p 8722:22 \
  -e PASSWORD=$R_PASS\
  -v $(pwd):/home/rstudio/bios611\
  -d\
@@ -37,6 +43,37 @@ box_out 'Log into RStudio on port 8787'\
         "Password: $R_PASS"\
         'to kill the docker container'\
         "run \"docker kill $CID\""
+
+fi
+
+if [ "$MODE" == "sshd" ];
+then
+export R_PASS=$(strings /dev/urandom | grep -o '[[:alnum:]]' | head -n 30 | tr -d '\n'; echo)
+docker run --rm \
+ -p 8722:22 \
+ -e PASSWORD=$R_PASS\
+ -v $(echo $HOME/.ssh):/home/rstudio/.ssh\
+ -v $(pwd):/home/rstudio/bios611\
+ -d\
+ -t 611\
+ /usr/sbin/sshd -D > /tmp/docker-output
+
+export DOUT=$(cat /tmp/docker-output)
+
+echo $DOUT
+
+export CID=$(echo $DOUT | tail -1)
+
+box_out 'ssh into the machine on port 8722'\
+        'Username: rstudio'\
+        "Password: $R_PASS"\
+        'to kill the docker container'\
+        "run \"docker kill $CID\""
+
+fi
+
+
+
 
 
  
